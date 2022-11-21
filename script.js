749be987e33578366930dc5d89d8f3c440b5fa4d
opju21 <117085376+opju21@users.noreply.github.com>
2022-11-21 23:17:44 +0100

    it has begun

---
+++ script.js
@@ -0,0 +1,4 @@
+const redButton = document.querySelector("#redButton");
+const greenButton = document.getElementById("greenButton");
+const blueButton = document.getElementById("blueButton");
+const bodyElement = document.querySelector("body");
