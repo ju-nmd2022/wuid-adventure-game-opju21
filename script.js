d4e980355bd3af0e7f3f0ca1931a3d32d6f23f86
opju21 <117085376+opju21@users.noreply.github.com>
2022-11-22 11:40:08 +0100

    functions lol

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-const redButton = document.querySelector("#redButton");
-const greenButton = document.getElementById("greenButton");
-const blueButton = document.getElementById("blueButton");
-const bodyElement = document.querySelector("body");
+const lArrow = document.querySelector("#lArrow");
+const rArrow = document.getElementById("rArrow");
+const box = document.querySelector(".box");
+
+lArrow.addEventListener("click", function (event) {
+  box.style.backgroundColor = "#FF0000";
+});
+rArrow.addEventListener("click", function (event) {
+  box.style.backgroundColor = "#00ff00";
+});
