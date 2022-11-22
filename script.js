32d8b701aaf07be11fda5a157c914e3aa294ee85
opju21 <117085376+opju21@users.noreply.github.com>
2022-11-22 14:16:01 +0100

    mapping out stuff

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
 const lArrow = document.querySelector("#lArrow");
 const rArrow = document.getElementById("rArrow");
 const box = document.querySelector(".box");
-
-lArrow.addEventListener("click", function (event) {
-  box.style.backgroundColor = "#FF0000";
-});
-rArrow.addEventListener("click", function (event) {
-  box.style.backgroundColor = "#00ff00";
-});
+const item = document.getElementsByClassName("item");
